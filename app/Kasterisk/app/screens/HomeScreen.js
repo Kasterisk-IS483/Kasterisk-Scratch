d6ccccf76fcb8960b04eedcfba568313644a9452
FionaWee <54692252+FionaWee@users.noreply.github.com>
2021-01-04 00:36:34 +0800

    refactor(M1): use expo-google-app-auth for google authentication instead of expo-app-auth

---
+++ app/Kasterisk/app/screens/HomeScreen.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text> Profile Screen </Text>
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+          Welcome, {this.props.navigation.getParam("idToken")}
+        </Text>
+        <Button
+          title="Sign out"
+          onPress={() => this.props.navigation.navigate("WelcomeScreen")}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
