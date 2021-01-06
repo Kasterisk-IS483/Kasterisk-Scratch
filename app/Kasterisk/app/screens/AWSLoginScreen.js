05764198841a2e58d1ef78c1eef8f91d19de4eb9
Madhumitha <43128631+madhumitha998@users.noreply.github.com>
2021-01-06 13:45:45 +0800

    refactor(M1): Excluding variable temporarily to fix bug

--- app/Kasterisk/app/screens/AWSLoginScreen.js
+++ app/Kasterisk/app/screens/AWSLoginScreen.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
-var AWS = require('aws-sdk/dist/aws-sdk-react-native');
+// var AWS = require('aws-sdk/dist/aws-sdk-react-native');
 
 export default function AWSLoginScreen({ navigation }) {
     return (
