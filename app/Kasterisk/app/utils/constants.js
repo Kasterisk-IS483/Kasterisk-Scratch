5e392cb995fee87d343765d35bd5deeefe4a55bf
trevordino <trevor.ng.2018@sis.smu.edu.sg>
2021-01-09 19:22:45 +0800

    refactor(M1): create constants.js

---
+++ app/Kasterisk/app/utils/constants.js
@@ -0,0 +1,2 @@
+export const IOS_CLIENT_ID = "587877229134-ivv7cc74rvke5jhhsejeq4cugpdfe0lh.apps.googleusercontent.com";
+export const ANDROID_CLIENT_ID = "587877229134-jlnds3fpb8i91orlrdstmedgvfus2qov.apps.googleusercontent.com";
