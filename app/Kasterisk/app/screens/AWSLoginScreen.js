1c607cd9bf022ccefd94993ffea93153c9802981
jasxmine <54893485+jasxmine@users.noreply.github.com>
2021-01-10 01:33:37 +0800

    Added aws authentication

--- app/Kasterisk/app/screens/AWSLoginScreen.js
+++ app/Kasterisk/app/screens/AWSLoginScreen.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { withAuthenticator } from 'aws-amplify-react-native'
+import Amplify from '@aws-amplify/core'
+import config from '../../aws-exports'
+Amplify.configure(config)
 
 // var AWS = require('aws-sdk/dist/aws-sdk-react-native');
 
-export default function AWSLoginScreen({ navigation }) {
-    return (
-        <View>
-            <Text>AWS Login</Text>
-        </View>
-    )
+class AWSLoginScreen extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>AWS Login</Text>
+            </View>
+        )
+    }
 }
+
+export default withAuthenticator(AWSLoginScreen, { includeGreetings: true })
