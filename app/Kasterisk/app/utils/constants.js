7d7828edbe7258dd4c8e810b57cb69adc0d39557
FionaWee <54692252+FionaWee@users.noreply.github.com>
2021-01-16 01:33:04 +0800

    refactor(M1): update client ids for google auth

--- app/Kasterisk/app/utils/constants.js
+++ app/Kasterisk/app/utils/constants.js
@@ -1,5 +1,5 @@
-export const IOS_CLIENT_ID = "587877229134-ivv7cc74rvke5jhhsejeq4cugpdfe0lh.apps.googleusercontent.com";
-export const ANDROID_CLIENT_ID = "587877229134-jlnds3fpb8i91orlrdstmedgvfus2qov.apps.googleusercontent.com";
+export const IOS_CLIENT_ID = "447181180888-bou5e3olte901t7srk0e1mv3c413g0lt.apps.googleusercontent.com";
+export const ANDROID_CLIENT_ID = "447181180888-ker4pf1d0d16u3d8pnek049q3kdpu07k.apps.googleusercontent.com";
 
 export var ClusterAuthProviderGoogle = {
     accessToken: "",
