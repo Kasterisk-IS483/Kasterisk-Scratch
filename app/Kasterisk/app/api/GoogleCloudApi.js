28775b526e8889ca3a326d8fd40bd11b515a77a3
FionaWee <54692252+FionaWee@users.noreply.github.com>
2021-01-16 23:57:29 +0800

    feat(M1): add google apis for google authentication

--- app/Kasterisk/app/api/GoogleCloudApi.js
+++ app/Kasterisk/app/api/GoogleCloudApi.js
@@ -1,5 +1,6 @@
 import * as Google from "expo-google-app-auth";
-import { IOS_CLIENT_ID, ANDROID_CLIENT_ID, ClusterAuthProviderGoogle, saveTemporaryCredentials } from "../utils/constants";
+import { IOS_CLIENT_ID, ANDROID_CLIENT_ID, WEB_CLIENT_ID, WEB_CLIENT_SECRET, 
+    ClusterAuthProviderGoogle, saveTemporaryCredentials } from "../utils/constants";
 
 class GoogleCloudApi {
 
@@ -34,6 +35,95 @@ class GoogleCloudApi {
             return { error: true };
         }
     };
+
+    static refreshAccessToken = async refreshToken => {
+        const url = 'https://oauth2.googleapis.com/token?' +
+          `client_id=${WEB_CLIENT_ID}&` +
+          `client_secret=${WEB_CLIENT_SECRET}&` +
+          `refresh_token=${refreshToken}&` +
+          'grant_type=refresh_token';
+    
+        const newAccessToken = await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        }).then(res => res.json());
+    
+        return newAccessToken.access_token;
+    };
+
+    static gcpFetch = async ({ url, method = 'get', body, refreshToken }) => {
+        const accessToken = await this.refreshAccessToken(refreshToken);
+
+        const headers = {
+            Authorization: `Bearer ${accessToken}`,
+        };
+
+        const res = await fetch(url, {
+            headers,
+            method,
+            body,
+        });
+
+        return res.json();
+    };
+
+    static fetchProjects = async ({ pageToken, refreshToken }) => {
+        const url = pageToken
+            ? `https://cloudresourcemanager.googleapis.com/v1/projects?pageSize=50&pageToken=${pageToken}`
+            : 'https://cloudresourcemanager.googleapis.com/v1/projects?pageSize=50';
+
+        try {
+            const projects = await this.gcpFetch({ url, refreshToken });
+            if (projects.error) {
+                return alert(projects.error.message);
+            }
+            return projects;
+        } catch (err) {
+            Promise.reject(err);
+        }
+    }
+
+    static fetchZones = async ({ projectId, pageToken, refreshToken }) => {
+        const url = pageToken
+            ? `https://compute.googleapis.com/compute/v1/projects/${projectId}/zones?pageToken=${pageToken}`
+            : `https://compute.googleapis.com/compute/v1/projects/${projectId}/zones`;
+
+        try {
+            const zones = await this.gcpFetch({ url, refreshToken });
+            if (zones.error) {
+                return alert(zones.error.message);
+            }
+            return zones;
+        } catch (err) {
+            return Promise.reject(err);
+        }
+    }
+
+    static fetchGkeClusters = async ({ projectId, zone = '-', refreshToken }) => {
+        try {
+            const url = `https://container.googleapis.com/v1/projects/${projectId}/locations/${zone}/clusters`;
+            const clusters = await this.gcpFetch({ url, refreshToken });
+            if (!clusters.clusters) { return []; }
+            const clusterList = clusters.clusters.map(cluster => {
+            return {
+                url: cluster.endpoint,
+                name: cluster.name,
+                status: cluster.status,
+                createdAt: cluster.createTime,
+                cloudProvider: 'gcp',
+                namespaces: [],
+            };
+            });
+            if (clusters.error) {
+                return alert(clusters.error.message);
+            }
+            return clusterList;
+        } catch (err) {
+            return Promise.reject(err);
+        }
+    }
 
 }
 
--- app/Kasterisk/app/utils/constants.js
+++ app/Kasterisk/app/utils/constants.js
@@ -1,5 +1,9 @@
+import * as SecureStore from 'expo-secure-store';
+
 export const IOS_CLIENT_ID = "447181180888-bou5e3olte901t7srk0e1mv3c413g0lt.apps.googleusercontent.com";
 export const ANDROID_CLIENT_ID = "447181180888-ker4pf1d0d16u3d8pnek049q3kdpu07k.apps.googleusercontent.com";
+export const WEB_CLIENT_ID = "447181180888-u710h5qo7vde690vk9t2j2rrlmiq094n.apps.googleusercontent.com";
+export const WEB_CLIENT_SECRET = "zPT9I0zE2E5pNlpLW_-at593";
 
 export var ClusterAuthProviderGoogle = {
     accessToken: "",
@@ -7,8 +11,6 @@ export var ClusterAuthProviderGoogle = {
     idToken: "",
     refreshToken: "",
 };
-
-import * as SecureStore from 'expo-secure-store';
 
 export const saveTemporaryCredentials = async(toSave, credentials) => {
     try {
