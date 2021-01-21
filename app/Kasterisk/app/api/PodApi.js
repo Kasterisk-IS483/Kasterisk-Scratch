72bc8bfc415b6d2ee16593ad9b5d3ce007af0a60
jasxmine <54893485+jasxmine@users.noreply.github.com>
2021-01-22 03:58:22 +0800

    refactor(M2): update pod API functions

--- app/Kasterisk/app/api/PodApi.js
+++ app/Kasterisk/app/api/PodApi.js
@@ -1,4 +1,4 @@
-// import { RNFetchBlob } from 'rn-fetch-blob';
+import RNFetchBlob from "react-native-fetch-blob";
 
 import React, { Component } from "react";
 class PodApi extends Component {
@@ -27,26 +27,32 @@ class PodApi extends Component {
     //         return Promise.reject(err);
     //     }
     // };
-    static apiFetch = async ({ apiUrl, method = 'get' }) => {
+    static apiFetch = async ({ apiUrl, method = 'get', body }) => {
 
-        state = {credentials : []};
+        //state = { credentials: [] };
         try {
-            const url = "http://127.0.0.1:32768/api/v1" + `${apiUrl}`;
-            const requestOptions = {
-                method: 'GET',
-                headers: {
-                    'client-certificate': this.state.credentials["client-certificate-data"],
-                    'client-key': this.state.credentials["client-key-data"]
-                },
-            };
+            let url = "https://small-test.run.haas-242.pez.pivotal.io:8443" + `${apiUrl}`;
 
-            let response = await fetch(url,requestOptions);
-            let json = await response.json();
+            //let url = "https://10.0.2.2:32768" + `${apiUrl}`;
+            // const requestOptions = {
+            //     method: 'GET',
+            //     headers: {
+            //         'client-certificate': this.state.credentials["client-certificate-data"],
+            //         'client-key': this.state.credentials["client-key-data"]
+            //     },
+            // };
 
-            return json;
+            const response = await RNFetchBlob.config({
+                trusty: true,
+            }).fetch("GET", url,
+                {
+                    Authorization:
+                        "Bearer eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vYXBpLnJ1bi5oYWFzLTI0Mi5wZXoucGl2b3RhbC5pbzo4NDQzL3Rva2VuX2tleXMiLCJraWQiOiJrZXktMSIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyNDdkYmE2MjM1MjQ0YjFkOGY1ZjEyMjIwMmFjNjZkMS1yIiwic3ViIjoiZTM5NzZjOTQtNjBjMC00OWI1LWJhMzYtYTJlNzFmZmVkYjk5IiwiaWF0IjoxNjExMjQwOTk2LCJleHAiOjE2MTEyNjI1OTYsImNpZCI6InBrc19jbHVzdGVyX2NsaWVudCIsImNsaWVudF9pZCI6InBrc19jbHVzdGVyX2NsaWVudCIsImlzcyI6Imh0dHBzOi8vYXBpLnJ1bi5oYWFzLTI0Mi5wZXoucGl2b3RhbC5pbzo4NDQzL29hdXRoL3Rva2VuIiwiemlkIjoidWFhIiwiYXVkIjpbIm9wZW5pZCIsInBrc19jbHVzdGVyX2NsaWVudCJdLCJncmFudGVkX3Njb3BlcyI6WyJvcGVuaWQiLCJyb2xlcyJdLCJhbXIiOlsicHdkIl0sImF1dGhfdGltZSI6MTYxMTI0MDk5NiwiZ3JhbnRfdHlwZSI6InBhc3N3b3JkIiwidXNlcl9uYW1lIjoic211LWZ5cCIsIm9yaWdpbiI6InVhYSIsInVzZXJfaWQiOiJlMzk3NmM5NC02MGMwLTQ5YjUtYmEzNi1hMmU3MWZmZWRiOTkiLCJyZXZfc2lnIjoiYzJjY2U0OGQifQ.z4HpRFLQ2Ppd-CYuctB_QjNtf1sl2U4xqylK1o5ZwcvLnXAHCS0nXtZxA-Ka3EZ2QAuA7FM7jmAj1QeRxpafZFB-eRHzOk4FP_pYQuNEK6-T89RQQOPyow206WfbxOK3TbbOGWmEUKsetqKQbYNsneDxPRhbqjq1aDdtnB89pgmh_ciXRIh8cEJAbF8RzTX7LE1Mkl9_gjbRXAkzmF-gslACDa-HLgIHIOqRRU3YgvSw-sn6slBLL3beyutS1D3ywpF_1DKJ1FC4-iKpkkSU4idM3hxOGtYco8U6hmDv2RJvjxD3OU4jWLtMEEzWzxPA9igjEJHZFgH3sw-lARXTlg",
+                    insecureSkipTLSVerify: false
+                }
+            );
+            return response.json();
 
-            // const response = await fetch("GET",`${url}${apiUrl}`).then(res => res.json());
-            //return response;
         }
         catch (err) {
             console.log(err);
@@ -54,14 +60,17 @@ class PodApi extends Component {
         }
     };
 
+
     static get(apiUrl) {
         return this.apiFetch({ method: 'get', apiUrl });
     }
+
 
     static listAllNamespace = async () => {
         const podsList = await this.get(`/api/v1/pods`);
         return podsList.items;
     }
 
-  }
+
+}
 export default PodApi;
