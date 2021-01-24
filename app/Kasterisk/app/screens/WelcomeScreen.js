08b3fdf40d68a21baa93428898b827a811377745
trevordino <trevor.ng.2018@sis.smu.edu.sg>
2021-01-24 16:45:17 +0800

    refactor(M2): fixed google oauth

--- app/Kasterisk/app/screens/WelcomeScreen.js
+++ app/Kasterisk/app/screens/WelcomeScreen.js
@@ -1,178 +1,182 @@
 import React, { Component } from "react";
 import {
-    View,
-    Text,
-    ScrollView,
-    ImageBackground,
-    Image,
-    Dimensions,
-    Alert,
-    Linking
+  View,
+  Text,
+  ScrollView,
+  ImageBackground,
+  Image,
+  Dimensions,
+  Alert,
+  Linking,
 } from "react-native";
 
 import CustomButton from "../components/CustomButton";
 import {
-    commonStyles,
-    landscapeStyles,
-    portraitStyles
+  commonStyles,
+  landscapeStyles,
+  portraitStyles,
 } from "../utils/styles.js";
 
-import { authorize } from 'react-native-app-auth';
-import * as AuthSession from 'expo-auth-session';
-import { openAuthSession } from 'azure-ad-graph-expo';
+import { authorize } from "react-native-app-auth";
+import * as AuthSession from "expo-auth-session";
+import { openAuthSession } from "azure-ad-graph-expo";
 // import {AzureInstance, AzureLoginView} from 'react-native-azure-ad-2'
-import GoogleCloudApi from '../api/GoogleCloudApi';
-import { AZURE_DOMAIN_HINT,
-    AZURE_CLIENT_ID,
-    AZURE_TENANT_ID,
-    AZURE_CLIENT_SECRET,
-    googleConfig } from "../utils/constants";
+import GoogleCloudApi from "../api/GoogleCloudApi";
+import {
+  AZURE_DOMAIN_HINT,
+  AZURE_CLIENT_ID,
+  AZURE_TENANT_ID,
+  AZURE_CLIENT_SECRET,
+  googleConfig,
+} from "../utils/constants";
 
 const azureConfig = {
-    clientId: AZURE_CLIENT_ID,
-    issuer: "https://login.microsoftonline.com/" + AZURE_TENANT_ID + "/v2.0",
-    scopes: ['openid', 'profile', 'email', 'offline_access'],
-    redirectUrl: AuthSession.makeRedirectUri(),
-    clientSecret: AZURE_CLIENT_SECRET,
-    domainHint: AZURE_DOMAIN_HINT,
-    prompt: 'login'
+  clientId: AZURE_CLIENT_ID,
+  issuer: "https://login.microsoftonline.com/" + AZURE_TENANT_ID + "/v2.0",
+  scopes: ["openid", "profile", "email", "offline_access"],
+  redirectUrl: AuthSession.makeRedirectUri(),
+  clientSecret: AZURE_CLIENT_SECRET,
+  domainHint: AZURE_DOMAIN_HINT,
+  prompt: "login",
 };
 
 export default class WelcomeScreen extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            orientation: "",
-            result: null,
-        };
-        Dimensions.addEventListener("change", (e) => {
-            this.setState(e.window);
-        });
+  constructor(props) {
+    super(props);
+    this.state = {
+      orientation: "",
+      result: null,
+    };
+    Dimensions.addEventListener("change", (e) => {
+      this.setState(e.window);
+    });
+  }
+
+  getOrientation() {
+    if (Dimensions.get("window").width > Dimensions.get("window").height) {
+      return "LANDSCAPE";
+    } else {
+      return "PORTRAIT";
     }
+  }
 
-    getOrientation() {
-        if (Dimensions.get("window").width > Dimensions.get("window").height) {
-            return "LANDSCAPE";
-        } else {
-            return "PORTRAIT";
-        }
+  getStyle() {
+    if (this.getOrientation() === "LANDSCAPE") {
+      return landscapeStyles;
+    } else {
+      return portraitStyles;
     }
+  }
 
-    getStyle() {
-        if (this.getOrientation() === "LANDSCAPE") {
-            return landscapeStyles;
-        } else {
-            return portraitStyles;
-        }
+  onLayout() {
+    this.setState({ orientation: this.getOrientation() });
+  }
+
+  GoogleLogin = async () => {
+    try {
+      const authState = await authorize(googleConfig);
+        alert(authState.scopes);
+    //   const refreshedState = await refresh(googleConfig, {
+    //     refreshToken: authState.refreshToken,
+    //   });
+
+      // const isValidCredentials = await (
+      //     GoogleCloudApi.checkGoogleCredentials()
+      // );
+      // if (isValidCredentials) {
+      //     this.props.navigation.navigate('Home'); //after Google login redirect to Home
+      // } else {
+      //     Alert.alert('Login Cancelled', 'Please enter your email and password to sign in.');
+      // }
+    } catch (e) {
+      alert(e.message);
+      Alert.alert(
+        "Invalid Credentials",
+        "Please enter valid email and password for your google account."
+      );
     }
+  };
 
-    onLayout() {
-        this.setState({ orientation: this.getOrientation() });
-    }
+  _handlePressAsync = async () => {
+    console.log(azureAdAppProps.redirectUrl);
+    let result = await openAuthSession(azureAdAppProps);
+    this.setState({ result });
+  };
 
-    GoogleLogin = async () => {
-        try {
-            let a = Linking.addEventListener("url", authorize(googleConfig))
-            alert(a)
-            const authState = await authorize(googleConfig);
-            alert(authState.accessToken())
-            const refreshedState = await refresh(googleConfig, {
-                refreshToken: authState.refreshToken
-              });
-              
-            // const isValidCredentials = await (
-            //     GoogleCloudApi.checkGoogleCredentials()
-            // );
-            // if (isValidCredentials) {
-            //     this.props.navigation.navigate('Home'); //after Google login redirect to Home
-            // } else {
-            //     Alert.alert('Login Cancelled', 'Please enter your email and password to sign in.');
-            // }
-        }
-        catch (e) {
-            alert(e)
-            Alert.alert('Invalid Credentials', 'Please enter valid email and password for your google account.');;
-        }
-    }
+  render() {
+    const { navigation } = this.props;
 
-    _handlePressAsync = async () => {
-        console.log(azureAdAppProps.redirectUrl);
-        let result = await openAuthSession(azureAdAppProps);
-        this.setState({ result });
-    }
+    return (
+      <View style={this.getStyle().container}>
+        <View style={this.getStyle().fillContainer}>
+          <ImageBackground
+            style={this.getStyle().fillContainer}
+            source={require("../assets/welcome-bg.png")}
+            imageStyle={{ resizeMode: "cover" }}
+          />
+          <Image
+            style={this.getStyle().logo}
+            source={require("../assets/kasterisk-logo.png")}
+          />
+          <Text style={this.getStyle().description}>
+            Access, manage and monitor your Kubernetes clusters.
+          </Text>
+        </View>
 
-    render() {
-        const { navigation } = this.props;
+        <View style={this.getStyle().primaryContainer}>
+          <ScrollView
+            contentContainerStyle={[
+              commonStyles.scrollView,
+              commonStyles.centralise,
+            ]}
+          >
+            <CustomButton
+              image={require("../assets/welcome-button-google.png")}
+              text="Log in With Google"
+              size="small"
+              onPress={this.GoogleLogin}
+            />
+            <CustomButton
+              image={require("../assets/welcome-button-aws.png")}
+              text="Log in With Amazon"
+              size="small"
+              onPress={() => navigation.navigate("AWS Login")}
+            />
+            <CustomButton
+              image={require("../assets/welcome-button-azure.png")}
+              text="Log in With Azure AD"
+              size="small"
+              onPress={this._handlePressAsync}
+            />
+            {this.state.result ? (
+              <Text>{JSON.stringify(this.state.result)}</Text>
+            ) : (
+              <Text>Nothing to see here.</Text>
+            )}
 
-        return (
-            <View style={this.getStyle().container}>
-                <View style={this.getStyle().fillContainer}>
-                    <ImageBackground
-                        style={this.getStyle().fillContainer}
-                        source={require("../assets/welcome-bg.png")}
-                        imageStyle={{ resizeMode: "cover" }}
-                    />
-                    <Image
-                        style={this.getStyle().logo}
-                        source={require("../assets/kasterisk-logo.png")}
-                    />
-                    <Text style={this.getStyle().description}>
-                        Access, manage and monitor your Kubernetes clusters.
-                    </Text>
-                </View>
+            <View style={commonStyles.divider} />
 
-                <View style={this.getStyle().primaryContainer}>
-                    <ScrollView contentContainerStyle={[commonStyles.scrollView, commonStyles.centralise]}>
-                        <CustomButton
-                            image={require("../assets/welcome-button-google.png")}
-                            text="Log in With Google"
-                            size="small"
-                            onPress={this.GoogleLogin}
-                        />
-                        <CustomButton
-                            image={require("../assets/welcome-button-aws.png")}
-                            text="Log in With Amazon"
-                            size="small"
-                            onPress={() => navigation.navigate("AWS Login")}
-                        />
-                        <CustomButton
-                            image={require("../assets/welcome-button-azure.png")}
-                            text="Log in With Azure AD"
-                            size="small"
-                            onPress={this._handlePressAsync}
-                        />
-                        {this.state.result ? (
-                            <Text>{JSON.stringify(this.state.result)}</Text>)
-                            : <Text>Nothing to see here.</Text>}
-
-                        <View style={commonStyles.divider} />
-
-                        <CustomButton
-                            image={require("../assets/welcome-button-kube.png")}
-                            text="Upload Kubeconfig File"
-                            size="small"
-                            onPress={() => navigation.navigate("KubeconfigUpload")}
-                        />
-                        <CustomButton
-                            image={require("../assets/welcome-button-kube.png")}
-                            text="Add Kubeconfig Content"
-                            size="small"
-                            onPress={() => navigation.navigate("KubeconfigContent")}
-                        />
-                        <CustomButton
-                            image={require("../assets/welcome-button-kube.png")}
-                            text="Test Screen"
-                            size="small"
-                            onPress={() => navigation.navigate("TestScreen")}
-                        />
-                    </ScrollView>
-                </View>
-            </View>
-
-
-        );
-    }
+            <CustomButton
+              image={require("../assets/welcome-button-kube.png")}
+              text="Upload Kubeconfig File"
+              size="small"
+              onPress={() => navigation.navigate("KubeconfigUpload")}
+            />
+            <CustomButton
+              image={require("../assets/welcome-button-kube.png")}
+              text="Add Kubeconfig Content"
+              size="small"
+              onPress={() => navigation.navigate("KubeconfigContent")}
+            />
+            <CustomButton
+              image={require("../assets/welcome-button-kube.png")}
+              text="Test Screen"
+              size="small"
+              onPress={() => navigation.navigate("TestScreen")}
+            />
+          </ScrollView>
+        </View>
+      </View>
+    );
+  }
 }
-
-
-
--- app/Kasterisk/app/utils/constants.js
+++ app/Kasterisk/app/utils/constants.js
@@ -6,8 +6,9 @@ export const GOOGLE_ANDROID_CLIENT = "447181180888-ker4pf1d0d16u3d8pnek049q3kdpu
 export const GOOGLE_IOS_CLIENT_ID = GOOGLE_IOS_CLIENT + ".apps.googleusercontent.com";
 export const GOOGLE_ANDROID_CLIENT_ID = GOOGLE_ANDROID_CLIENT + ".apps.googleusercontent.com";
 export const GOOGLE_IOS_REDIRECT = "com.googleusercontent.apps." + GOOGLE_IOS_CLIENT + ":/oauth2redirect/google";
-export const GOOGLE_ANDROID_REDIRECT = "com.googleusercontent.apps." + GOOGLE_ANDROID_CLIENT +":/oauth2redirect/google";
-// export const GOOGLE_ANDROID_REDIRECT = "com.kasterisk.android:/oauth2redirect/google"
+// export const GOOGLE_ANDROID_REDIRECT = "com.googleusercontent.apps." + GOOGLE_ANDROID_CLIENT +":/oauth2redirect/google";
+export const GOOGLE_ANDROID_REDIRECT = "com.kasterisk.android" + ":/oauth2redirect/google";
+
 
 
 export const WEB_CLIENT_ID = "447181180888-u710h5qo7vde690vk9t2j2rrlmiq094n.apps.googleusercontent.com";
@@ -19,11 +20,15 @@ export const AZURE_CLIENT_SECRET = 'K0Hsw1-jnPb5iQ7~5S9V.q3zID7fg5~.lB';
 export const AZURE_DOMAIN_HINT = '';
 
 export const googleConfig = {
-  issuer: 'https://accounts.google.com',
+  issuer: 'https://accounts.google.com/',
+  // serviceConfiguration: {
+  //   authorizationEndpoint: "https://accounts.google.com/o/oauth2/v2/auth",
+  //   tokenEndpoint: "https://oauth2.googleapis.com/token",
+  //   revocationEndpoint: "https://oauth2.googleapis.com/revoke"
+  // },
   clientId: Platform.OS === 'ios' ? GOOGLE_IOS_CLIENT_ID : GOOGLE_ANDROID_CLIENT_ID,
   redirectUrl: Platform.OS === 'ios' ? GOOGLE_IOS_REDIRECT : GOOGLE_ANDROID_REDIRECT,
-  scopes: ['openid', 'profile', 'https://www.googleapis.com/auth/cloud-platform'],
-  additionalParameters: { 'response-type': 'code' },
+  scopes: ['openid', 'email', 'profile', 'https://www.googleapis.com/auth/cloud-platform']
 };
 
 
