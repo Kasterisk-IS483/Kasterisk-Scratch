1963c2e79d0a0f16380e79e04753e83dbef6c1ee
FionaWee <54692252+FionaWee@users.noreply.github.com>
2021-01-25 10:56:04 +0800

    feat(M2): added other methods for DeploymentApi

--- app/Kasterisk/app/api/DeploymentApi.js
+++ app/Kasterisk/app/api/DeploymentApi.js
@@ -3,7 +3,77 @@ import CommonAPI from "./CommonApi.js";
 
 class DeploymentApi extends Component {
 
-    //GET FUNCTIONS
+    /** WRITE **/
+
+    /**
+     * Create Deployment
+     * POST /api/v1/namespaces/{namespace}/deployments
+     * 
+     * @param namespace 
+     * @body Deployment
+     */
+    static createDeployment = async (namespace, body) => {
+        return await CommonAPI.post(`/api/v1/namespaces/${namespace}/deployments`, body);
+    }
+
+
+    /**
+     * Patch Deployment
+     * PATCH /apis/apps/v1/namespaces/{namespace}/deployments/{name}
+     * 
+     * @param namespace 
+     * @param name
+     * @body patch
+     */
+    static patchDeployment = async (namespace, name, body) => {
+        const deploymentsList = await CommonAPI.patch(`/apis/apps/v1/namespaces/${namespace}/deployments/${name}`, body);
+        return deploymentsList.items;
+    }
+
+
+    /**
+     * Replace Deployment
+     * PUT /apis/apps/v1/namespaces/{namespace}/deployments/{name}
+     * 
+     * @param namespace 
+     * @param name
+     * @body Deployment
+     */
+    static replaceDeployment = async (namespace, name, body) => {
+        const deploymentsList = await CommonAPI.put(`/apis/apps/v1/namespaces/${namespace}/deployments/${name}`, body);
+        return deploymentsList.items;
+    }
+
+
+    /**
+     * Delete Deployment
+     * DELETE /apis/apps/v1/namespaces/{namespace}/deployments/{name}
+     * 
+     * @param namespace 
+     * @param name
+     * @body DeleteOptions
+     */
+    static deleteDeployment = async (namespace, name, body) => {
+        const deploymentsList = await CommonAPI.delete(`/apis/apps/v1/namespaces/${namespace}/deployments/${name}`, body);
+        return deploymentsList.items;
+    }
+
+
+    /**
+     * Delete Collection of Deployments
+     * DELETE /apis/apps/v1/namespaces/{namespace}/deployments
+     * 
+     * @param namespace 
+     * @param name
+     * @body DeleteOptions
+     */
+    static deleteDeploymentCollection = async (namespace, body) => {
+        const deploymentsList = await CommonAPI.delete(`/apis/apps/v1/namespaces/${namespace}/deployments`, body);
+        return deploymentsList.items;
+    }
+
+
+    /** READ **/
 
     /**
      * list or watch objects of kind Deployment in all namespaces
@@ -41,6 +111,22 @@ class DeploymentApi extends Component {
     }
 
 
+    /** STATUS **/
+
+    /**
+     * Patch Deployment Status
+     * PATCH /apis/apps/v1/namespaces/{namespace}/deployments/{name}/status
+     * 
+     * @param namespace 
+     * @param name
+     * @body Patch
+     */
+    static patchDeploymentStatus = async (namespace, name, body) => {
+        const DeploymentsList = await CommonAPI.patch(`/apis/apps/v1/namespaces/${namespace}/deployments/${name}/status`, body);
+        return DeploymentsList.items;
+    }
+
+
     /**
      * read status of the specified Deployment
      * GET /apis/apps/v1/namespaces/{namespace}/deployments/{name}/status
@@ -52,6 +138,22 @@ class DeploymentApi extends Component {
         const DeploymentStatus = await CommonAPI.get(`/apis/apps/v1/namespaces/${namespace}/deployments/${name}/status`);
         return DeploymentStatus;
     }
+
+
+    /**
+     * Replace Deployment Status
+     * PUT /apis/apps/v1/namespaces/{namespace}/deployments/{name}/status
+     * 
+     * @param namespace 
+     * @param name
+     * @body Deployment
+     */
+    static replaceDeploymentStatus = async (namespace, name, body) => {
+        const DeploymentsList = await CommonAPI.get(`/apis/apps/v1/namespaces/${namespace}/deployments/${name}/status`, body);
+        return DeploymentsList.items;
+    }
+    
+    /** MISC **/
 
     /**
      * read scale of the specified Deployment
@@ -66,6 +168,32 @@ class DeploymentApi extends Component {
     }
 
 
+    /**
+     * Replace Deployment Scale
+     * PUT /apis/apps/v1/namespaces/{namespace}/deployments/{name}/scale
+     * 
+     * @param namespace 
+     * @param name
+     * @body Scale
+     */
+    static replaceDeploymentScale = async (namespace, name, body) => {
+        const DeploymentsList = await CommonAPI.get(`/apis/apps/v1/namespaces/${namespace}/deployments/${name}/scale`, body);
+        return DeploymentsList.items;
+    }
+    
+
+    /**
+     * Patch Deployment Scale
+     * PATCH /apis/apps/v1/namespaces/{namespace}/deployments/{name}/scale
+     * 
+     * @param namespace 
+     * @param name
+     * @body Patch
+     */
+    static patchDeploymentScale = async (namespace, name, body) => {
+        const DeploymentsList = await CommonAPI.get(`/apis/apps/v1/namespaces/${namespace}/deployments/${name}/scale`, body);
+        return DeploymentsList.items;
+    }
 
 }
 export default DeploymentApi;
