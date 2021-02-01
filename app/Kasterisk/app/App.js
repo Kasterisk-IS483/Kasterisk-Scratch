363993aba1d959a9f8ef9a1730f6996486682b6f
trevordino <trevordino@gmail.com>
2021-02-01 20:18:05 +0800

    refactor(M3): update iOS splash screen

--- app/Kasterisk/app/App.js
+++ app/Kasterisk/app/App.js
@@ -1,7 +1,8 @@
 import "react-native-gesture-handler";
-import React from "react";
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import SplashScreen from 'react-native-splash-screen'
 
 import WelcomeScreen from "./screens/WelcomeScreen";
 import KubeconfigUploadScreen from "./screens/KubeconfigUploadScreen";
@@ -18,6 +19,11 @@ const Stack = createStackNavigator();
 const MORE_ICON = Platform.OS === "ios" ? "dots-horizontal" : "dots-vertical";
 
 export default function App() {
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator
