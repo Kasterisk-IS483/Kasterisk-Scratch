201fd555cba37906a02f65c35e0e600d2de22caf
Constance <hytan.2018@sis.smu.edu.sg>
2021-02-08 14:32:07 +0800

    feat(M3): hamburger menu & app routing

--- app/Kasterisk/app/App.js
+++ app/Kasterisk/app/App.js
@@ -2,6 +2,7 @@ import "react-native-gesture-handler";
 import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import SplashScreen from 'react-native-splash-screen'
 
 import WelcomeScreen from "./screens/WelcomeScreen";
@@ -9,20 +10,35 @@ import KubeconfigUploadScreen from "./screens/KubeconfigUploadScreen";
 import KubeconfigContentScreen from "./screens/KubeconfigContentScreen";
 import AWSLoginScreen from "./screens/AWSLoginScreen";
 import WorkloadSummaryScreen from "./screens/WorkloadSummaryScreen";
-import TestScreen from "./screens/TestScreen";
 import LoadingScreen from "./screens/LoadingScreen";
+import TestScreen from "./screens/TestScreen";
 
 import { colours } from "./utils/styles";
 
 const AuthContext = React.createContext();
 
-
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 const MORE_ICON = Platform.OS === "ios" ? "dots-horizontal" : "dots-vertical";
 
-export default function App({ navigation }) {
-  const AuthContext = 
+const screenOptions = {
+  headerTintColor: "white",
+  headerStyle: { backgroundColor: colours.primary },
+  headerShown: true,
+}
+
+function Home() {
+  return (
+    <Drawer.Navigator initialRouteName="Welcome" screenOptions={screenOptions} >
+      <Drawer.Screen name="Home" component={WelcomeScreen} options={{ headerShown: false }} />
+      <Drawer.Screen name="WorkloadSummary" component={WorkloadSummaryScreen} options={{ title: "Workloads" }} />
+      <Drawer.Screen name="Namespaces" component={LoadingScreen} options={{ title: "Namespaces" }} />
+    </Drawer.Navigator>
+  );
+}
+
+export default function App() {
 
   useEffect(() => {
     SplashScreen.hide();
@@ -30,19 +46,17 @@ export default function App({ navigation }) {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerStyle: { backgroundColor: colours.primary },
-          headerTintColor: "white"
-      }}>
-        <Stack.Screen name="Home" component={WelcomeScreen} options={{ headerShown: false }} />
+      <Stack.Navigator initialRouteName="Home" screenOptions={screenOptions}>
+        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+
+        {/* Welcome Screen Buttons */}
         <Stack.Screen name="AWS Login" component={AWSLoginScreen} />
-        <Stack.Screen name="KubeconfigUpload" component={KubeconfigUploadScreen} options={{title: "Upload Kubeconfig File"}} />
-        <Stack.Screen name="KubeconfigContent" component={KubeconfigContentScreen} options={{title: "Add Kubeconfig Content"}} />
-        {/* Testing Screens */}
-        <Stack.Screen name="WorkloadSummary" component={WorkloadSummaryScreen} options={{title: "Workloads"}} />
-        <Stack.Screen name="Loading" component={LoadingScreen} options={{title: "Loading"}} />
-        <Stack.Screen name="Test" component={TestScreen} />
+        <Stack.Screen name="KubeconfigUpload" component={KubeconfigUploadScreen} options={{ title: "Upload Kubeconfig File" }} />
+        <Stack.Screen name="KubeconfigContent" component={KubeconfigContentScreen} options={{ title: "Add Kubeconfig Content" }} />
+
+        {/* Misc */}
+        {/* <Stack.Screen name="Loading" component={LoadingScreen} options={{ title: "Loading" }} /> */}
+        {/* <Stack.Screen name="Test" component={TestScreen} /> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
