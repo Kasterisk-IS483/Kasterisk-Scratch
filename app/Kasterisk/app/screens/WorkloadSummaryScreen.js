2ca8d4d6544eb44e59fe4d7613c17323c1eb6efb
jasxmine <54893485+jasxmine@users.noreply.github.com>
2021-02-17 01:25:57 +0800

    feat(m3): add template filter into WorkloadSummaryScreen

--- app/Kasterisk/app/screens/WorkloadSummaryScreen.js
+++ app/Kasterisk/app/screens/WorkloadSummaryScreen.js
@@ -1,11 +1,13 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import React, { Component } from "react";
-import { View, ScrollView, Dimensions, Alert } from "react-native";
+import React, { Component, useState } from "react";
+import { View, ScrollView, Dimensions, Alert, Text, Icon } from "react-native";
 import { TabView, TabBar } from 'react-native-tab-view';
 import Spinner from "react-native-loading-spinner-overlay";
+import { Picker } from "@react-native-picker/picker";
 
 import { checkServerStatus } from '../api/KubeApi'
 import AwsApi from "../api/AwsApi";
+import { AWSRegions } from "../utils/constants";
 import DeploymentApi from "../api/DeploymentApi";
 import ReplicasetApi from "../api/ReplicasetApi";
 import PodApi from "../api/PodApi";
@@ -21,6 +23,7 @@ import OverviewCard from "../components/Cards/OverviewCard";
 import WorkloadCard from "../components/Cards/WorkloadCard";
 import LabelButton from "../components/Buttons/LabelButton";
 
+
 export default class WorkloadSummaryScreen extends Component {
 
   constructor(props) {
@@ -35,12 +38,15 @@ export default class WorkloadSummaryScreen extends Component {
         { key: 'third', title: 'Replicaset' },
         { key: 'fourth', title: 'Pod' },
       ],
-      spinner: false
+      spinner: false,
+      region: ""
     };
     Dimensions.addEventListener("change", (e) => {
       this.setState(e.window);
     });
   }
+
+
 
   getOrientation() {
     if (Dimensions.get("window").width > Dimensions.get("window").height) {
@@ -62,6 +68,16 @@ export default class WorkloadSummaryScreen extends Component {
     this.setState({ orientation: this.getOrientation() });
   }
 
+  NamespaceList() {
+    return (
+      <Picker 
+        selectedValue={this.region} onValueChange={(itemValue, itemIndex) => setRegion(itemValue)} >
+        {AWSRegions.map((_item, _index) => (
+          <Picker.Item label={_item.label} value={_item.value} key={_item.value} />
+        ))}
+      </Picker>
+    );
+  };
   //state = { credentials: [] };
 
   // async getGoogle(){
@@ -146,12 +162,12 @@ export default class WorkloadSummaryScreen extends Component {
     } catch (err) {
       Alert.alert("Server Check Failed", err.message);
     }
-    
+
     this.setState({
       spinner: false
     })
     // await this.listAllTest();
-    
+
   }
 
 
@@ -169,43 +185,67 @@ export default class WorkloadSummaryScreen extends Component {
   _renderScene = ({ route }) => {
     switch (route.key) {
       case 'first':
-        return <View style={this.getStyle().dashboardRowContainer}>
-          <View style={this.getStyle().dashboardCardColumnContainer}>
-            <OverviewCard 
-              image={require("../assets/deployment.png")} 
-              name="Deployment" 
-              text1="Ready"
-              text2="Not Ready"
-              no1="2"
-              no2="0"
-            />
+        return <View>
+          <View>
+            <Text
+              style={[
+                commonStyles.formSectionHeaader,
+                {
+                  marginHorizontal: spacings.xxl,
+                  paddingHorizontal: spacings.sm,
+                },
+              ]}
+            >
+              Select Namespace:
+            </Text>
+            <View
+              style={{
+                borderWidth: 1,
+                borderColor: "black",
+                marginTop: spacings.sm,
+                marginHorizontal: 40
+              }}>
+              {this.NamespaceList()}
+            </View>
           </View>
-          <View style={this.getStyle().dashboardCardColumnContainer}>
-            <OverviewCard 
-              image={require("../assets/replicaset.png")} 
-              name="ReplicaSet"
-              text1="Ready"
-              text2="Not Ready"
-              no1="2"
-              no2="1"
-            />
-          </View>
-          <View style={this.getStyle().dashboardCardColumnContainer}>
-            <OverviewCard 
-              image={require("../assets/pod.png")} 
-              name="Pod" 
-              text1="Running"
-              text2="Pending"
-              no1="3"
-              no2="1"
-            />
-          </View>
-        </View>;
+          <View style={this.getStyle().dashboardRowContainer}>
+            <View style={this.getStyle().dashboardCardColumnContainer}>
+
+              <OverviewCard
+                image={require("../assets/deployment.png")}
+                name="Deployment"
+                text1="Ready"
+                text2="Not Ready"
+                no1="2"
+                no2="0"
+              />
+            </View>
+            <View style={this.getStyle().dashboardCardColumnContainer}>
+              <OverviewCard
+                image={require("../assets/replicaset.png")}
+                name="ReplicaSet"
+                text1="Ready"
+                text2="Not Ready"
+                no1="2"
+                no2="1"
+              />
+            </View>
+            <View style={this.getStyle().dashboardCardColumnContainer}>
+              <OverviewCard
+                image={require("../assets/pod.png")}
+                name="Pod"
+                text1="Running"
+                text2="Pending"
+                no1="3"
+                no2="1"
+              />
+            </View>
+          </View></View>;
 
       case 'second':
         return <View style={commonStyles.dashboardContainer}>
-          <WorkloadCard 
-            name="test" 
+          <WorkloadCard
+            name="test"
             age="0"
             status="4"
             total="4"
@@ -217,8 +257,8 @@ export default class WorkloadSummaryScreen extends Component {
             <LabelButton text="testinge" />
             <LabelButton text="ab" />
           </WorkloadCard>
-          <WorkloadCard 
-            name="test2" 
+          <WorkloadCard
+            name="test2"
             age="0"
             status="3"
             total="4"
@@ -230,8 +270,8 @@ export default class WorkloadSummaryScreen extends Component {
             <LabelButton text="testinge" />
             <LabelButton text="ab" />
           </WorkloadCard>
-          <WorkloadCard 
-            name="test3" 
+          <WorkloadCard
+            name="test3"
             age="0"
             status="0"
             total="2"
@@ -243,8 +283,8 @@ export default class WorkloadSummaryScreen extends Component {
             <LabelButton text="test" />
             <LabelButton text="ab" />
           </WorkloadCard>
-          <WorkloadCard 
-            name="test4" 
+          <WorkloadCard
+            name="test4"
             age="0"
             status="3"
             total="3"
@@ -256,11 +296,11 @@ export default class WorkloadSummaryScreen extends Component {
             <LabelButton text="test" />
             <LabelButton text="testinge" />
           </WorkloadCard>
-          <WorkloadCard 
-            name="test5" 
+          <WorkloadCard
+            name="test5"
             age="0"
-            status="2" 
-            total="4" 
+            status="2"
+            total="4"
             variableField="Containers"
             variableFieldVal="test3"
           >
@@ -268,36 +308,36 @@ export default class WorkloadSummaryScreen extends Component {
             <LabelButton text="test" />
             <LabelButton text="testinge" />
           </WorkloadCard>
-          <WorkloadCard 
-              name="test6" 
-              age="0"
-              status="1" 
-              total="4" 
-              variableField="Containers"
-              variableFieldVal="test4"
-            >
-              <LabelButton text="app:hellonode" />
-              <LabelButton text="test" />
-              <LabelButton text="testinge" />
-              <LabelButton text="abcde" />
-            </WorkloadCard>
-          </View>;
+          <WorkloadCard
+            name="test6"
+            age="0"
+            status="1"
+            total="4"
+            variableField="Containers"
+            variableFieldVal="test4"
+          >
+            <LabelButton text="app:hellonode" />
+            <LabelButton text="test" />
+            <LabelButton text="testinge" />
+            <LabelButton text="abcde" />
+          </WorkloadCard>
+        </View>;
 
       case 'third':
         return <View style={commonStyles.dashboardContainer}>
-          <WorkloadCard 
-            name="hello-node" 
-            label="app:hellonode" 
+          <WorkloadCard
+            name="hello-node"
+            label="app:hellonode"
             age="0"
             status="4"
             total="4"
             variableField="Containers"
             variableFieldVal="echoserve"
-            >
+          >
           </WorkloadCard>
-          <WorkloadCard 
-            name="test" 
-            label="app:hellonode" 
+          <WorkloadCard
+            name="test"
+            label="app:hellonode"
             age="0"
             status="3"
             total="4"
@@ -305,9 +345,9 @@ export default class WorkloadSummaryScreen extends Component {
             variableFieldVal="container"
           >
           </WorkloadCard>
-          <WorkloadCard 
-            name="test" 
-            label="app:hellonode" 
+          <WorkloadCard
+            name="test"
+            label="app:hellonode"
             age="0"
             status="0"
             total="2"
@@ -315,9 +355,9 @@ export default class WorkloadSummaryScreen extends Component {
             variableFieldVal="test"
           >
           </WorkloadCard>
-          <WorkloadCard 
-            name="hello-node2" 
-            label="app:hellonode" 
+          <WorkloadCard
+            name="hello-node2"
+            label="app:hellonode"
             age="0"
             status="3"
             total="3"
@@ -325,12 +365,12 @@ export default class WorkloadSummaryScreen extends Component {
             variableFieldVal="test2"
           >
           </WorkloadCard>
-          <WorkloadCard 
-            name="test" 
-            label="app:hellonode" 
+          <WorkloadCard
+            name="test"
+            label="app:hellonode"
             age="0"
-            status="2" 
-            total="4" 
+            status="2"
+            total="4"
             variableField="Containers"
             variableFieldVal="test3"
           >
@@ -339,36 +379,36 @@ export default class WorkloadSummaryScreen extends Component {
 
       case 'fourth':
         return <View style={commonStyles.dashboardContainer}>
-          <WorkloadCard 
-            name="hello-node" 
+          <WorkloadCard
+            name="hello-node"
             age="0"
-            status="Running" 
+            status="Running"
             variableField="Restarts"
             variableFieldVal="0"
           >
             <LabelButton text="app:hellonode" />
           </WorkloadCard>
-          <WorkloadCard 
-            name="hello-node" 
+          <WorkloadCard
+            name="hello-node"
             age="0"
-            status="Running" 
+            status="Running"
             variableField="Restarts"
             variableFieldVal="0"
           >
             <LabelButton text="app:hellonode" />
             <LabelButton text="test" />
           </WorkloadCard>
-          <WorkloadCard 
-            name="hello-node" 
+          <WorkloadCard
+            name="hello-node"
             age="0"
-            status="Running" 
+            status="Running"
             variableField="Restarts"
             variableFieldVal="0"
           />
-          <WorkloadCard 
-            name="hello-node" 
+          <WorkloadCard
+            name="hello-node"
             age="0"
-            status="Running" 
+            status="Running"
             variableField="Restarts"
             variableFieldVal="0"
           >
@@ -376,22 +416,10 @@ export default class WorkloadSummaryScreen extends Component {
             <LabelButton text="test" />
             <LabelButton text="testinge" />
           </WorkloadCard>
-          <WorkloadCard 
-            name="hello-node" 
+          <WorkloadCard
+            name="hello-node"
             age="0"
-            status="Running" 
-            variableField="Restarts"
-            variableFieldVal="0"
-          >
-            <LabelButton text="app:hellonode" />
-            <LabelButton text="test" />
-            <LabelButton text="testinge" />
-            <LabelButton text="abcde" />
-          </WorkloadCard>
-          <WorkloadCard 
-            name="hello-node" 
-            age="0"
-            status="Running" 
+            status="Running"
             variableField="Restarts"
             variableFieldVal="0"
           >
@@ -400,20 +428,10 @@ export default class WorkloadSummaryScreen extends Component {
             <LabelButton text="testinge" />
             <LabelButton text="abcde" />
           </WorkloadCard>
-          <WorkloadCard 
-            name="hello-node" 
+          <WorkloadCard
+            name="hello-node"
             age="0"
-            status="Pending" 
-            variableField="Restarts"
-            variableFieldVal="0"
-          >
-            <LabelButton text="app:hellonode" />
-            <LabelButton text="test" />
-          </WorkloadCard>
-          <WorkloadCard 
-            name="hello-node" 
-            age="0"
-            status="Pending" 
+            status="Running"
             variableField="Restarts"
             variableFieldVal="0"
           >
@@ -422,34 +440,56 @@ export default class WorkloadSummaryScreen extends Component {
             <LabelButton text="testinge" />
             <LabelButton text="abcde" />
           </WorkloadCard>
-          <WorkloadCard 
-            name="hello-node" 
+          <WorkloadCard
+            name="hello-node"
             age="0"
-            status="Pending" 
+            status="Pending"
+            variableField="Restarts"
+            variableFieldVal="0"
+          >
+            <LabelButton text="app:hellonode" />
+            <LabelButton text="test" />
+          </WorkloadCard>
+          <WorkloadCard
+            name="hello-node"
+            age="0"
+            status="Pending"
+            variableField="Restarts"
+            variableFieldVal="0"
+          >
+            <LabelButton text="app:hellonode" />
+            <LabelButton text="test" />
+            <LabelButton text="testinge" />
+            <LabelButton text="abcde" />
+          </WorkloadCard>
+          <WorkloadCard
+            name="hello-node"
+            age="0"
+            status="Pending"
             variableField="Restarts"
             variableFieldVal="0"
           >
           </WorkloadCard>
-          <WorkloadCard 
-            name="hello-node" 
+          <WorkloadCard
+            name="hello-node"
             age="0"
-            status="Pending" 
+            status="Pending"
             variableField="Restarts"
             variableFieldVal="0"
           >
           </WorkloadCard>
-            <WorkloadCard 
-            name="hello-node" 
+          <WorkloadCard
+            name="hello-node"
             age="0"
-            status="Pending" 
+            status="Pending"
             variableField="Restarts"
             variableFieldVal="0"
           >
           </WorkloadCard>
-          <WorkloadCard 
-            name="hello-node" 
+          <WorkloadCard
+            name="hello-node"
             age="0"
-            status="Pending" 
+            status="Pending"
             variableField="Restarts"
             variableFieldVal="0"
           >
@@ -465,9 +505,9 @@ export default class WorkloadSummaryScreen extends Component {
     return (
       <ScrollView style={commonStyles.secondaryContainer}>
         <Spinner
-            visible={this.state.spinner}
-            textContent={"Loading..."}
-            textStyle={{ color: '#FFF' }}
+          visible={this.state.spinner}
+          textContent={"Loading..."}
+          textStyle={{ color: '#FFF' }}
         />
         <TabView
           navigationState={this.state}
