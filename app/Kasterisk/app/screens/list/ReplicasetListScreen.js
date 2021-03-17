0f44625e1ef9aedb436fac26dcb282149ddcf220
jasxmine <54893485+jasxmine@users.noreply.github.com>
2021-03-18 01:41:03 +0800

    Feat(M5): Add replicaset screen list

---
+++ app/Kasterisk/app/screens/list/ReplicasetListScreen.js
@@ -0,0 +1,117 @@
+import React, { Component } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { ScrollView, Dimensions, Alert } from "react-native";
+import Spinner from "react-native-loading-spinner-overlay";
+
+import { checkServerStatus } from "../../api/KubeApi";
+
+import TableCard from "../../components/Cards/TableCard";
+import WorkloadSummaryApi from "../../api/WorkloadSummaryApi";
+
+import {
+  colours,
+  commonStyles,
+  dashboardStyles,
+  commonPortraitStyles,
+  workloadDetailsBreakpoint,
+  cardsOuterPadding,
+  spacings,
+} from "../../utils/styles.js";
+
+export default class ReplicasetListScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      orientation: "",
+      replicasets: [],
+      replicasetsArr: [],
+      namespace: "",
+      spinner: false,
+    };
+    Dimensions.addEventListener("change", (e) => {
+      this.setState(e.window);
+    });
+  }
+
+  getOrientation() {
+    if (Dimensions.get("window").width > workloadDetailsBreakpoint) {
+      return "LANDSCAPE";
+    } else {
+      return "PORTRAIT";
+    }
+  }
+  getStyle() {
+    if (this.getOrientation() === "LANDSCAPE") {
+      return commonStyles;
+    } else {
+      return commonPortraitStyles;
+    }
+  }
+  onLayout() {
+    this.setState({ orientation: this.getOrientation() });
+  }
+
+  formatObject(item) {
+    const status = item["status"];
+    const total = item["total"];
+    item["status"] = status +"/" + total;
+    delete item.total;
+    this.setState({
+      replicasetsArr: [...this.state.replicasetsArr, Object.values(item)],
+    });
+    console.log(this.state.replicasets)
+  }
+  async componentDidMount() {
+    const { navigation } = this.props;
+    this.setState({
+      spinner: true,
+    });
+
+    if ((await AsyncStorage.getItem("@selectedValue")) != null) {
+      this.setState({
+        namespace: await AsyncStorage.getItem("@selectedValue"),
+      });
+    }
+
+    try {
+      let defaultCluster = await AsyncStorage.getItem("@defaultCluster");
+      console.log(defaultCluster);
+
+      if (defaultCluster == null) {
+        Alert.alert("Error", "Default cluster not found");
+        this.setState({
+          spinner: false,
+        });
+        this.props.navigation.navigate("ChooseCluster");
+        return;
+      }
+
+      let serverStatus = await checkServerStatus(defaultCluster);
+      console.log(serverStatus);
+      if (serverStatus[0] == 200) {
+        this.setState({
+          replicasets: await WorkloadSummaryApi.replicasetsInfo(
+            this.state.namespace
+          ),
+        });
+        this.state.replicasets.map((item, index) => this.formatObject(item));
+
+      } else {
+        Alert.alert("Error", "Failed to contact cluster");
+      }
+    } catch (err) {
+      Alert.alert("Server Check Failed", err.message);
+    }
+    this.setState({
+      spinner: false,
+    });
+  }
+
+  render() {
+    return (
+      <ScrollView style={commonStyles.secondaryContainer}>
+        <TableCard header="Replicasets List" table={this.state.replicasetsArr} />
+      </ScrollView>
+    );
+  }
+}
