92ff0b2b893e08611516ca3cf2b0257f561b4fea
Madhumitha <43128631+madhumitha998@users.noreply.github.com>
2021-03-22 18:15:52 +0800

    feat(M5): Add setLabels to changeClusterScreen

--- app/Kasterisk/app/screens/authentication/ChangeClusterScreen.js
+++ app/Kasterisk/app/screens/authentication/ChangeClusterScreen.js
@@ -9,19 +9,56 @@ import { commonStyles, fonts } from "../../utils/styles.js";
 import SubmitButton from "../../components/Buttons/SubmitButton";
 import SpinnerOverlay from "../../components/Elements/SpinnerOverlay";
 
+import { checkServerStatus } from "../../api/KubeApi";
+import WorkloadSummaryApi from "../../api/WorkloadSummaryApi";
+
 class ChangeClusterScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
       spinner: true,
       loaded: false,
+      namespaceLabels: ["All Namespaces"],
     };
     this.clusterList = [];
     this.previousCluster = null;
     this.previousClusterData = null;
+    this.setLabels = this.setLabels.bind(this);
+  }
+
+  async setLabels(defaultCluster) {
+    console.log("setLabels");
+    this.setState({
+      spinner: true,
+    });
+    try {
+      // let defaultCluster = await AsyncStorage.getItem("@defaultCluster");
+      // console.log(defaultCluster);
+      // if (defaultCluster != null) {
+        let serverStatus = await checkServerStatus(defaultCluster);
+        console.log(serverStatus);
+        if (serverStatus[0] == 200) {
+          await AsyncStorage.setItem("@selectedValue", "");
+          //Not working
+          this.setState({
+            namespaceLabels: await WorkloadSummaryApi.namespaceLabels(),
+          });
+        } else {
+          Alert.alert("Error", "Failed to contact cluster");
+        }
+      // }
+      console.log("/setLabels");
+      console.log(this.state.namespaceLabels);
+    } catch (err) {
+      Alert.alert("Server Check Failed", err.message);
+    }
+    this.setState({
+      spinner: false,
+    });
   }
 
   async componentDidMount() {
+    console.log("ChangeClusterScreen.js mounted");
     // Check if there are any clusters stored in localstorage.
     // if none, redirect to add cluster page
     let allClusters = await AsyncStorage.getItem("@clusters");
@@ -39,6 +76,7 @@ class ChangeClusterScreen extends Component {
     // if yes, redirect to workload summary page
     let defaultCluster = await AsyncStorage.getItem("@defaultCluster");
     if (defaultCluster != null) {
+      console.log("there is default cluster");
       this.setState({ spinner: false });
       this.props.navigation.reset({
         index: 0,
@@ -57,6 +95,7 @@ class ChangeClusterScreen extends Component {
     } catch (err) { }
 
     if (this.previousCluster != null) {
+      console.log("there is prev cluster");
       this.previousClusterData = JSON.parse(await AsyncStorage.getItem(this.previousCluster));
     }
 
@@ -72,7 +111,7 @@ class ChangeClusterScreen extends Component {
 
   render() {
     const { navigation } = this.props;
-
+    console.log("ChangeClusterScreen.js render");
     return (
       <View style={commonStyles.whiteContainer}>
         <SpinnerOverlay showSpinner={this.state.spinner} />
@@ -87,6 +126,7 @@ class ChangeClusterScreen extends Component {
                 left={(props) => <List.Icon {...props} icon="aws" />}
                 onPress={async () => {
                   await AsyncStorage.setItem("@defaultCluster", "@" + this.previousClusterData.clusterIdentifier);
+                  await this.setLabels("@" + this.previousClusterData.clusterIdentifier);
                   this.props.navigation.reset({
                     index: 0,
                     routes: [{ name: "HomeDrawer", params: { screen: "WorkloadSummary" } }],
@@ -108,6 +148,7 @@ class ChangeClusterScreen extends Component {
               left={(props) => <List.Icon {...props} icon={aCluster.serviceProvider} />}
               onPress={async () => {
                 await AsyncStorage.setItem("@defaultCluster", "@" + aCluster.clusterIdentifier);
+                await this.setLabels("@" + aCluster.clusterIdentifier);
                 this.props.navigation.navigate("HomeDrawer", { screen: "WorkloadSummary" });
               }}
             />
