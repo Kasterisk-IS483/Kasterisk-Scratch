c440b9e8f99f9bd97e31aff43f8b5daf5116adfa
jasxmine <54893485+jasxmine@users.noreply.github.com>
2021-03-26 04:31:14 +0800

    Feat(M5): Added temp ModalDropdown for Filter label

--- app/Kasterisk/app/components/Templates/WorkloadTemplate.js
+++ app/Kasterisk/app/components/Templates/WorkloadTemplate.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { View, ScrollView, Title } from "react-native";
+import { View, ScrollView, Text } from "react-native";
+import { Card, Title } from "react-native-paper";
+import ModalDropdown from "react-native-modal-dropdown";
 
-import { commonStyles, dashboardStyles } from "../../utils/styles.js";
+import { commonStyles, dashboardStyles, fonts, spacings} from "../../utils/styles.js";
 import TableCard from "../Cards/TableCard";
 import SpinnerOverlay from "../Elements/SpinnerOverlay";
 
@@ -10,6 +12,39 @@ export default function WorkloadTemplate(props) {
     <ScrollView style={dashboardStyles.scrollContainer}>
       <SpinnerOverlay showSpinner={props.showSpinner} />
       <View style={commonStyles.detailsContainer}>
+        {props.type=="filter"&& <View style={commonStyles.wrapCard}>
+        <Card elevation={10} width={"100%"}>
+          <Card.Content style={commonStyles.cardContent}>
+            <Title style={commonStyles.cardTitle}>Filter</Title>
+          </Card.Content>
+  
+          <Card.Content style={commonStyles.cardContent}>
+          <View style={{ flexDirection: "row" }}>
+          <ModalDropdown
+            // options={namespaceLabels}
+            dropdownStyle={{
+              // height: 40 * namespaceLabels.length,
+              height:40,
+              alignItems: "center",
+            }}
+            dropdownTextStyle={{ fontSize: fonts.sm, color: "black" }}
+            textStyle={{
+              fontSize: fonts.sm,
+              color: "white",
+              marginRight: spacings.xxs,
+            }}
+            customButton="⋮"
+            // defaultValue={this.state.selectedNamespace}
+            // onSelect={async (index) =>
+            //   this.updateState(namespaceLabels[index])
+            // }
+          />
+        <Text style={{ color: "white", marginRight: spacings.sm }}>▼</Text>
+      </View>
+            
+          </Card.Content>
+        </Card>
+      </View>}
         {props.type=="filter"&& <TableCard header={"Deployments List"} table={props.deployment} />}
         {props.type=="filter"&& <TableCard header={"Pods List"} table={props.pods} />}
         {props.type=="filter"&& <TableCard header={"Replicasets List"} table={props.replicasets} />}
