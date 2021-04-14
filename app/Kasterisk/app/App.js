7609b6ab073346ca49df310307716f1d740b2e19
Constance <hytan.2018@sis.smu.edu.sg>
2021-04-15 02:41:14 +0800

    fix(M6): add back navigation to app.js for google login / projects

--- app/Kasterisk/app/App.js
+++ app/Kasterisk/app/App.js
@@ -20,6 +20,7 @@ import { spacings, colours, fonts, commonStyles } from "./utils/styles";
 import WelcomeScreen from "./screens/authentication/WelcomeScreen";
 import KubeconfigUploadScreen from "./screens/authentication/KubeconfigUploadScreen";
 import KubeconfigContentScreen from "./screens/authentication/KubeconfigContentScreen";
+import GoogleLoginScreen from "./screens/authentication/GoogleLoginScreen";
 import AWSLoginScreen from "./screens/authentication/AWSLoginScreen";
 import AddClusterScreen from "./screens/authentication/AddClusterScreen";
 // workload 
@@ -526,6 +527,10 @@ export default class App extends Component {
             name="welcomeScreen"
             component={WelcomeScreen}
             options={{ headerShown: false }}
+          />
+          <Stack.Screen name="GoogleLogin"
+            component={GoogleLoginScreen}
+            options={{ title: "Google Login" }}
           />
           <Stack.Screen name="AWSLogin"
             component={AWSLoginScreen}
